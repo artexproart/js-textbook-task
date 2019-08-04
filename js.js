f4be6a843e0a0e712f4062d6a78956a547c8c2ad
Your Name <kirillovaaru@yandex.ru>
2019-08-04 22:23:22 +0300

    Дана строка с цифрами, например, '12345'. Проверьте, что первым символом этой строки является цифра 1, 2 или 3. Если это так - выведите 'да', в противном случае выведите 'нет'.

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
-let arr = ['a', 'b', 'c', 'd', 'e'];
-if (arr[0] === 'a') {
+let arr = [1, 2, 3, 4, 5];
+if (arr[0] === 1 && arr[1] === 2 && arr[2] === 3) {
     console.log('Ok');
 } else {
     console.log('No');
