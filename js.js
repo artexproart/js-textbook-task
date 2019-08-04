84330d65a59047b9a0a71d70b0a92101ab6ec18c
Your Name <kirillovaaru@yandex.ru>
2019-08-04 22:18:33 +0300

    В переменной day лежит какое-то число из интервала от 1 до 31. Определите в какую декаду месяца попадает это число (в первую, вторую или третью).

--- js.js
+++ js.js
@@ -1,16 +1,8 @@
-let num = 3;
-switch (num) {
-    case 1:
-        result = 'winter';
-    break;
-    case 2:
-        result = 'spring';
-    break;
-    case 3:
-        result = 'summer';
-    break;
-    case 4:
-        result = 'autumm';
+let day = 22; 
+if (day >= 1 && day <= 11) {
+    console.log('first');
+} else if (day >= 12 && day <= 21) { 
+    console.log('second');
+} else if (day >= 22 && day <= 31) {
+    console.log('third');
 }
-
-console.log(result);
