83bc99f404c459df407c21ef379f3aebe92119f1
Your Name <kirillovaaru@yandex.ru>
2019-08-04 22:11:28 +0300

    Переменная num может принимать 4 значения: 1, 2, 3 или 4. Если она имеет значение '1', то в переменную result запишем 'зима', если имеет значение '2' – 'весна' и так далее. Решите задачу через switch-case.

--- js.js
+++ js.js
@@ -1,7 +1,9 @@
-var a = 9;
-var b = 8;
-if (a >= 2 && a <= 11 || b >= 6 && b < 14) {
-    console.log('ok');
-} else {
-    console.log('no');
+let num = 1;
+switch (num) {
+    case num == 1:
+        result = winter;
+    break;
+    case num != 1:
+        result = summer;
 }
+console.log(result); 
