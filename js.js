d23e3958e6ab82be0e6fbce4158f4394a6f80938
Your Name <kirillovaaru@yandex.ru>
2019-08-04 22:20:21 +0300

    В переменной month лежит какое-то число из интервала от 1 до 12. Определите в какую пору года попадает этот месяц (зима, лето, весна, осень).

--- js.js
+++ js.js
@@ -1,8 +1,11 @@
-let day = 22; 
-if (day >= 1 && day <= 11) {
-    console.log('first');
-} else if (day >= 12 && day <= 21) { 
-    console.log('second');
-} else if (day >= 22 && day <= 31) {
-    console.log('third');
+let month = 5;
+if (month <= 2 || month == 12) {
+    season = 'winter';
+} else if (month >=3 || month <= 5) {
+    season = 'spring';
+} else if (monf >= 6 || month <= 8) {
+    season = 'sommer';
+} else if (month >= 9 || month <= 11) {
+    season = 'autumn';
 }
+console.log(season); 
