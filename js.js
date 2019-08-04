3155e687363025d89ad8363e374273c7f0b75af2
Your Name <kirillovaaru@yandex.ru>
2019-08-04 22:22:10 +0300

    Дана строка, состоящая из символов, например, 'abcde'. Проверьте, что первым символом этой строки является буква 'a'. Если это так - выведите 'да', в противном случае выведите 'нет'.

--- js.js
+++ js.js
@@ -1,11 +1,6 @@
-let month = 5;
-if (month <= 2 || month == 12) {
-    season = 'winter';
-} else if (month >=3 || month <= 5) {
-    season = 'spring';
-} else if (monf >= 6 || month <= 8) {
-    season = 'sommer';
-} else if (month >= 9 || month <= 11) {
-    season = 'autumn';
+let arr = ['a', 'b', 'c', 'd', 'e'];
+if (arr[0] === 'a') {
+    console.log('Ok');
+} else {
+    console.log('No');
 }
-console.log(season); 
